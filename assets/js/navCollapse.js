35b4226995ecc266b0a276cfd5276aee2f0ade98
Cristine Baumgarten <cristine.baumgarten@issinc.com>
2015-11-05 15:56:10 -0700

    collapsejs file added

---
+++ assets/js/navCollapse.js
@@ -0,0 +1,16 @@
+ /**
+     * Collapse the nav (facets/filters)
+     */
+    function NavCollapse() {
+        $('.nav-expanded').hide();
+        $('.nav-collapsed').show();
+		}
+	/**
+     * Expand the nav (facets/filters)
+     */
+    function NavExpand() {
+        $('.nav-expanded').show();
+        $('.nav-collapsed').hide();
+    }
+
+		
